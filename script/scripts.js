0840bbb7ba9470745799f5ff186e0e0466a270ec
Murilo Machado Hoffman de Oliveira <165489051+MuriloMHO@users.noreply.github.com>
2026-04-09 13:47:08 +0100

    New Dropdown
    Updated the Dropdown method to be based on JS and no longer based on Hover

--- script/scripts.js
+++ script/scripts.js
@@ -1,9 +1,12 @@
-let items = document.querySelector(".dropdown_content")
-
 let clickMenu = function() {
-    if (items.style.display === 'none' || items.style.display === '') {
+    const items = document.querySelector(".dropdown_content");
+    const botton = document.querySelector(".dropbtn");
+    
+    if (items.style.display === 'none' || items.style.display === "") {
         items.style.display = 'block'
+        botton.innerHTML = "X";
     } else {
         items.style.display = 'none'
+        botton.innerHTML = "☰";
     }
 }
