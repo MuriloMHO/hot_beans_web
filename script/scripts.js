a23287faffca7af8dd1ecb0a953c9d439120c8f3
Murilo Machado Hoffman de Oliveira <165489051+MuriloMHO@users.noreply.github.com>
2026-04-15 14:59:17 +0100

    Mass Update

--- script/scripts.js
+++ script/scripts.js
@@ -9,4 +9,27 @@ let clickMenu = function() {
         items.style.display = 'none'
         botton.innerHTML = "☰";
     }
-}
+};
+
+const form = document.getElementById("application_form");
+const message = document.getElementById("response")
+
+form.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
+
+    const formData = {
+        first: document.getElementById("first").value,
+        last: document.getElementById("last").value,
+        email: document.getElementById("email").value,
+        mobile: document.getElementById("mobile").value,
+        role: document.getElementById("role").value
+    };
+
+    console.log("Form Submitted!", formData);
+
+    message.style.color = "green";
+    message.textContent = `Thank you, ${formData.first}! Your application for ${formData.role} has been submitted`;
+
+    form.reset();
+});
